86eefef5e6843726efb304b06657a31e1b0e4bc5
MiloFisher <tbfmilo@gmail.com>
2021-10-18 19:26:38 -0700

    Misspelled path

--- puzzlePlatform/main.js
+++ puzzlePlatform/main.js
@@ -248,7 +248,7 @@ function update() {
 }
 
 function readRoomTemplates() {
-  var file = "platformPuzzle/roomTemplates.json";
+  var file = "puzzlePlatform/roomTemplates.json";
   var rawFile = new XMLHttpRequest();
   rawFile.overrideMimeType("application/json");
   rawFile.open("GET", file, true);
